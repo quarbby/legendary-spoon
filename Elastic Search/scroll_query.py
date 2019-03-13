88c2cc7adbfcab02503edda729b91f4c387ad6c8
weichaooo <weichao1995@gmail.com>
2019-03-13 17:34:33 +0800

    Simplified the script

--- Elastic Search/scroll_query.py
+++ Elastic Search/scroll_query.py
@@ -1,3 +1,12 @@
+"""
+Query with a scroll id
+- Allows displaying of results more than limit
+- Maximum limit: size = 10000
+
+After query, place results into a DATAFRAME.
+returns a DATAFRAME
+"""
+
 import pandasticsearch
 import pandas as pd
 import numpy as np
@@ -12,48 +21,34 @@ def processing_hits(res):
 	df = Select.from_dict(res).to_pandas()
 	return df
 
-"""
-Query with a scroll id
-- Allows displaying of results more than limit
-- Maximum limit: size = 10000
-
-After query, place results into a DATAFRAME.
-returns a DATAFRAME
-"""
-
 def query(key_word):
 
-	df = pd.DataFrame()
-	df['index'] = np.nan
 
 	#Query from all indexes available
 
-	for i in range(len(indexes)):
-		res = es.search(index = indexes[i], size = 10, scroll = '2m', body = {"query" : {
-			"match" : {"summary" : key_word}
-			}})
+	res = es.search(size = 10, scroll = '2m', body = {"query" : {
+		"match" : {"summary" : key_word}
+		}})
 
-		#Get the scroll id
+	#Get the scroll id
+	sid = res['_scroll_id']
+	scroll_size = len(res['hits']['hits'])
+
+	#Put first half of data into a dataframe
+	df = processing_hits(res)
+	# df = df.insert(index = indexes[i])
+
+	#Scroll and append into the dataframe
+	while scroll_size > 0 :
+		res = es.scroll(scroll_id = sid, scroll = '2m')
+		df = df.append(processing_hits(res), sort = True)
 		sid = res['_scroll_id']
 		scroll_size = len(res['hits']['hits'])
+		# df = df.insert(index, indexes[i])
 
-		#Put first half of data into a dataframe
-		df = df.append(processing_hits(res), sort = True)
-		# df = df.insert(index = indexes[i])
-
-		#Scroll and append into the dataframe
-		while scroll_size > 0 :
-			res = es.scroll(scroll_id = sid, scroll = '2m')
-			df = df.append(processing_hits(res), sort = True)
-			sid = res['_scroll_id']
-			scroll_size = len(res['hits']['hits'])
-			# df = df.insert(index, indexes[i])
-
-		df.loc[df['index'].isnull(), 'index'] = indexes[i]
-
-		#reset the index and print the dataframe
-		df = df.reset_index(drop = True)
-		# print(df.summary)
+	#reset the index and print the dataframe
+	df = df.reset_index(drop = True)
+	# print(df.summary)
 
 	return (df)
 
@@ -62,3 +57,7 @@ Example on using this function:
 df = query('machine')
 print(df)
 """
+
+df = query('economics')
+print(df.iloc[1])
+# print(df)
