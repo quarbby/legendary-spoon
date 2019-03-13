330f86673d42d3eb6823508657a3169c0659d9fd
weichaooo <weichao1995@gmail.com>
2019-03-13 17:33:12 +0800

    Changed the variables for config

--- Elastic Search/config.py
+++ Elastic Search/config.py
@@ -1,9 +1,17 @@
-#Change the param to where your data set is
-# location = 'D:/test/scholar_paper_02.json'
-location = 'D:/test/News_1_labelled.json'
+"""
+Config File consists of the following:
+1) Location of data file to be uploaded.
+2) Connection to Elastic Search
+"""
+from elasticsearch import Elasticsearch
 
-import pandas as pd
+"""
+Change the param to where your data set is.
+Example: location = 'D:/test/scholar_paper_02.json'
+"""
+location = 'D:/test/scholar_paper_02.json'
 
-df = pd.DataFrame()
-
-print(df)
+"""
+Establish connection to Elastic search
+"""
+es = Elasticsearch([{'host' : 'localhost', 'port' : 9200}])
