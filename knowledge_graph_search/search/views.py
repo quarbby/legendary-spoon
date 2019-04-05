62e53d98d97c784eaf8817409a55c79c305316c8
wesleychew <wes8814@hotmail.com>
2019-04-05 09:58:55 +0800

    Techscan 1.0

--- knowledge_graph_search/search/views.py
+++ knowledge_graph_search/search/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .ElasticSearchFolder import scroll_query, paper_count, plotgraph
-from .Neo4jFolder import neo4j_ace_query, neo4j_main_query, neo4jgraph
+from .Neo4jFolder import neo4jgraph
 import json
 from textblob import TextBlob
 
@@ -9,6 +9,11 @@ def chi_keyword(keyword):
 	word = TextBlob(str(keyword))
 	chinese = word.translate(to = 'zh')
 	return (str(chinese))
+
+def eng_keyword(keyword):
+	word = TextBlob(str(keyword))
+	english = word.translate(to = 'en')
+	return (str(english))
 
 def index(request):
 	return render(request, 'index.html')
@@ -21,28 +26,28 @@ def search_word(request):
 		# plotgraph.multi_year(params, 'scholar')
 		# plotgraph.count_date(params, 'news')
 		# plotgraph.count_date(params, 'tweets')
+		# plotgraph.count_date(chi_keyword(params), 'weibo')
+
 
 		_,es_result_news = scroll_query.text_query(params,'news')
 		_,es_result_scholar = scroll_query.text_query(params,'scholar')
 		_,es_result_tweets = scroll_query.text_query(params,'tweets',15)
 		_,es_result_weibo = scroll_query.text_query(chi_keyword(params),'weibo',15)
-		neo4jgraph.plot_neo4j_graph()
-		# neo4j_field_result = neo4j_ace_query.related_field(params)
-		# neo4j_author_result = neo4j_ace_query.related_author(params)
-		# neo4j_paper_result = neo4j_ace_query.related_paper(params)
+		# graph,_ = neo4jgraph.plotgraph(neo4jgraph.search_field(params))
+		
+		eng_summary = [eng_keyword(list(es_result_weibo)[i]['summary']) for i in range(4)]
 
 		context = {
 			"searchTerm": params,
 			"elasticSearchNews": es_result_news,
 			"elasticSearchScholar": es_result_scholar,
 			"elasticSearchTweets": es_result_tweets,
-			"elasticSearchWeibo": es_result_weibo,
-			# "neo4j_field": json.loads(neo4j_field_result),
-			# "neo4j_author" : json.loads(neo4j_author_result),
-			"chiKeyword": chi_keyword(params)
-			# "neo4j_paper" : json.loads(neo4j_paper_result),
+			"elasticSearchWeibo": zip(es_result_weibo,eng_summary),
+			"chiKeyword": chi_keyword(params),
+			# "translate": eng_summary,
 		}
 		return render(request, 'results.html', context)
+		# return HttpResponse(eng_summary)
 		
 	# except:
 		# 
