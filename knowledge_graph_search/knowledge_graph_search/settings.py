4aeeadf09b8693f024dfcf213a39bdca7074b0c2
Lorenzo <lorenzo.ong@berkeley.edu>
2019-04-07 05:13:12 +0800

    connecting react with django

--- knowledge_graph_search/knowledge_graph_search/settings.py
+++ knowledge_graph_search/knowledge_graph_search/settings.py
@@ -42,7 +42,7 @@ ROOT_URLCONF = 'knowledge_graph_search.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates"), ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -110,3 +110,12 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_URL = '/static/'
+
+# React stuff
+
+WEBPACK_LOADER = {
+    'DEFAULT': {
+            'BUNDLE_DIR_NAME': 'bundles/',
+            'STATS_FILE': os.path.join(BASE_DIR, 'webpack-stats.dev.json'),
+        }
+}
--- knowledge_graph_search/search/urls.py
+++ knowledge_graph_search/search/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 from django.views.generic import TemplateView
+from django.conf.urls import url
 
 urlpatterns = [
     path('',views.index),
@@ -11,7 +12,7 @@ urlpatterns = [
     path('plotlinescholar/', TemplateView.as_view(template_name="basic_line_graph_scholar.html"),name='plotlinescholar'),
     path('plotneo4jgraph/', TemplateView.as_view(template_name="neo4jgraph.html"), name= 'plotneo4jgraph'),
     path('plotlineweibo/', TemplateView.as_view(template_name="basic_line_graph_weibo.html"),name='plotlineweibo'),
-
+    url(r'^', TemplateView.as_view(template_name="index.html")),
 ]
 
  
